ed5e348b227efee7ae5403b9967a5bbbed9ff81f
bella01111 <bella.bkn011@gmail.com>
2024-12-19 20:49:31 +0700

    pertemuan 12

---
+++ resources/views/home.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.main')
+@section('title', 'Home Page')
+@section('content')
+    <div class="container-fluid">
+        <div class="container-fluid">
+            <div class="row page-titles mx-0">
+                <div class="col-sm-6 p-md-0">
+                    <div class="welcome-text">
+                        <h4>Rental Mobil</h4>
+                    </div>
+                </div>
+            </div>
+            <d class="row">
+                @foreach ($mobils as $mobil)
+                    <div class="col-xl-4 col-xxl-6 col-lg-6 col-sm-6">
+                        <div class="card mb-3">
+                            <img class="card-img-top img-fluid" src="{{ asset('/storage/' . $mobil->foto) }}"
+                                style="width: auto; height: 300px;" alt="Card image cap">
+                            <div class="card-header">
+                                <h5 class="card-title">{{ $mobil->merek }}</h5>
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text text-dark">Alamat : {{ $mobil->rentalMobil->alamat }}</p>
+                                <p class="card-text text-dark">Kontak Owner : {{ $mobil->rentalMobil->no_hp }}</p>
+                                <a href="#" class="btn btn-primary float-right">Check</a>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,71 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MobilController;
+use App\Http\Controllers\UserController;  
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+// Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+
+Route::prefix('auth')->group(function () {
+
+    Route::middleware(['auth'])->group(function () {
+        Route::get('logout', [AuthController::class, 'logout'])->name('auth.logout');
+    });
+
+    Route::middleware(['guest'])->group(function () {
+        Route::get('login', [AuthController::class, 'login'])->name('auth.login');
+        Route::post('login', [AuthController::class, 'post'])->name('auth.login');
+
+        Route::get('register', [AuthController::class, 'register'])->name('auth.register');
+        Route::post('register', [AuthController::class, 'store'])->name('auth.register');
+    });
+});
+
+
+
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+
+
+
+
+Route::middleware(['auth', 'checkUserDetail'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+
+
+    Route::prefix('users')->group(function () {
+        Route::get('index', [UserController::class, 'index'])->name('users.index')->middleware('checkRole:SAD');
+        Route::get('profile/{id}', [UserController::class, 'profile'])->name('users.profile');
+        Route::put('profile/{id}', [UserController::class, 'update'])->name('users.profile');
+    });
+
+    Route::prefix('mobil')->middleware('checkRole:ADM')->group(function () {
+        Route::get('index', [MobilController::class, 'index'])->name('mobil.index');
+        Route::get('create', [MobilController::class, 'create'])->name('mobil.create');
+        Route::post('create', [MobilController::class, 'store'])->name('mobil.creare');
+        Route::get('edit/{id}', [MobilController::class, 'edit'])->name('mobil.edit');
+        Route::put('edit/{id}', [MobilController::class, 'update'])->name('mobil.update');
+        Route::delete('delete/{id}', [MobilController::class, 'destroy'])->name('mobil.delete');
+    });
+});
